c9a944fc1e92ef118e8a65410984232bfa39ccf5
forbelskym22 <mforbelak@gmail.com>
2026-01-31 12:14:06 +0100

    dashboard design

--- SBCWeb/src/routes/indexRouter.js
+++ SBCWeb/src/routes/indexRouter.js
@@ -2,7 +2,15 @@ const express = require('express');
 const router = express.Router();
 const indexController = require('../controllers/indexController');
 
-// Hlavní stránka
+// Hlavní stránka (login)
 router.get('/', indexController.getIndex);
+
+// Dashboard
+router.get('/dashboard', (req, res) => {
+  res.render('dashboard', { 
+    title: 'Dashboard',
+    projectName: 'IoT Control Panel'
+  });
+});
 
 module.exports = router;
