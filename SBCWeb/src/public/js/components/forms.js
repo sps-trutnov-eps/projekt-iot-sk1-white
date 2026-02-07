7069e1cca3d92d6fc75e752e939095fc9eebfbda
forbelskym22 <mforbelak@gmail.com>
2026-02-07 21:18:52 +0100

    animace toast message

--- SBCWeb/src/public/js/components/forms.js
+++ SBCWeb/src/public/js/components/forms.js
@@ -1,5 +1,42 @@
 document.addEventListener('DOMContentLoaded', () => {
     
+
+    const toast = document.getElementById("toast");
+    const toastMsg = document.getElementById("toast-message");
+    
+        let toastTimeout;
+
+    function openToast(message) {
+
+        toastMsg.innerHTML = message;
+        
+        clearTimeout(toastTimeout);
+
+        toast.classList.remove("hidden");
+
+        setTimeout(() => {
+            toast.classList.remove("opacity-0");
+            toast.classList.add("opacity-100");
+        }, 10); 
+
+        toastTimeout = setTimeout(() => {
+            
+            toast.classList.remove("opacity-100");
+            toast.classList.add("opacity-0");
+
+            setTimeout(() => {
+                toast.classList.add("hidden");
+            }, 1000);
+            
+        }, 2500);
+    }
+
+    
+
+    openToast("test");
+
+
+
     /*
     MCU formulář
     */
