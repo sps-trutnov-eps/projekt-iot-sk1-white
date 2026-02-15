ad016b662f17ff50228de2ac16e177bfeeaac187
forbelskym22 <mforbelak@gmail.com>
2026-02-15 20:03:21 +0100

    dekompozice mcuDetail

---
+++ SBCWeb/src/public/js/pages/mcuDetail/main.js
@@ -0,0 +1,29 @@
+import { initChart, updateChart, updateChartMetric } from './chartManager.js';
+import { loadSensors, fetchMcuInfo, initModals, removeMetric } from './sensorManager.js';
+
+// 1. GLOBÁLNÍ BRIDGE (Nezbytné pro onclick v HTML)
+window.updateChart = updateChart;
+window.updateChartMetric = updateChartMetric;
+window.removeMetric = removeMetric;
+window.refreshSensors = loadSensors;
+
+window.updateView = async function(isBackground = false) {
+    await fetchMcuInfo();
+    await loadSensors(isBackground);
+}
+
+// 2. START
+document.addEventListener('DOMContentLoaded', async () => {
+    // Toast logika
+    const msg = sessionStorage.getItem('toastMessage');
+    if (msg && window.openToast) {
+        window.openToast(msg, sessionStorage.getItem('toastSuccess') === 'true');
+        sessionStorage.clear();
+    }
+
+    initChart();
+    initModals();
+    await window.updateView(false);
+
+    setInterval(() => window.updateView(true), 5000);
+});
